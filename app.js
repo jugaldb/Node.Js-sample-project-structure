72b26c6b3aea837344e371aadf3bf261a8381a21
Jugal D. Bhatt <55304795+jugaldb@users.noreply.github.com>
2022-06-15 10:27:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use('/admin', adminRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  res.status(err.status || 404).json({
+  res.status(404).json({
     message: "No such route exists"
   })
 });
